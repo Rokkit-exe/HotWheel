587b36fafa81bc532f0e5daea45d2b3ba72d8886
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-05 14:01:53 -0400

    FDF update

--- mouvement.py
+++ mouvement.py
@@ -12,7 +12,7 @@ class Mouvement:
         self.ENB = gpiozero.PWMOutputDevice(18)
         self.initialise()
 
-    def forward(self, speed=25, wait=3):
+    def forward(self, speed=10, wait=2):
         fade_in = (100 - speed) / 1000
         fade_out = speed / 1000
         print(fade_in)
