efb75a9c12fdd3e29528ac08c51bb5d487e81a26
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-05 16:12:26 -0400

    test 2 HotWheel

--- mouvement.py
+++ mouvement.py
@@ -19,8 +19,8 @@ class Mouvement:
         print(fade_out)
         self.IN1.on()
         self.IN3.on()
-        self.ENA.blink(on_time=fade_in, off_time=fade_out, fade_in_time=1)
-        self.ENB.blink(on_time=fade_in, off_time=fade_out, fade_in_time=1)
+        #self.ENA.blink(on_time=fade_in, off_time=fade_out, fade_in_time=1)
+        #self.ENB.blink(on_time=fade_in, off_time=fade_out, fade_in_time=1)
         time.sleep(wait)
         self.initialise()
 
