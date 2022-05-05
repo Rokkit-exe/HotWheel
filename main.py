294ab1517b09a01e0bb4542b0a78302b18144126
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-05 16:03:48 -0400

    test 2 HotWheel

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ graphe = Graphe(matrice_distances)
 
 M = Mouvement()
 Infra = InfraRouge()
-
-M.forward(100, 1);
+while(True):
+    M.turn90("right", 1);
 
 
 #print(graphe.plus_court_chemin(0, 5)) # [0, 2, 3, 4, 5]
