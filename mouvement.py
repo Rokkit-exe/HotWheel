56097b6fb3f6fb3fff082b494da71d8ff8660687
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-05 17:14:59 -0400

    test

--- mouvement.py
+++ mouvement.py
@@ -28,8 +28,8 @@ class Mouvement:
     def turn90(self, dir, wait=1, condition=False):
         #while not condition:
             if (dir == "left"):
-                self.IN2.on()
-                self.IN3.on()
+                self.IN1.on()
+                self.IN4.on()
                 self.ENA.on()
                 self.ENB.on()
             elif (dir == "right"):
