a0fb2cc44c1975d164e166e013a26e1028d9019c
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-05 11:57:17 -0400

    FDF update mouvement

--- mouvement.py
+++ mouvement.py
@@ -1,15 +1,15 @@
-from gpiozero import *
+import gpiozero
 import time
 
 class Mouvement:
     def __init__(self):
-        self.IN1 = DigitalOutputDevice(6)
-        self.IN2 = DigitalOutputDevice(5)  # moteur G
-        self.ENA = PWDOutputDevice(13)
+        self.IN1 = gpiozero.DigitalOutputDevice(6)
+        self.IN2 = gpiozero.DigitalOutputDevice(5)  # moteur G
+        self.ENA = gpiozero.PWDOutputDevice(13)
 
-        self.IN3 = DigitalOutputDevice(15)
-        self.IN4 = DigitalOutputDevice(14) # mouteur
-        self.ENB = PWDOutputDevice(18)
+        self.IN3 = gpiozero.DigitalOutputDevice(15)
+        self.IN4 = gpiozero.DigitalOutputDevice(14) # mouteur
+        self.ENB = gpiozero.PWDOutputDevice(18)
 
     def forward(self, time=0):
         self.IN1.on()
