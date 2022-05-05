0e130552950736195c6fd6838f839f7129ef96b2
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-05 12:11:58 -0400

    FDF update mouvement

--- mouvement.py
+++ mouvement.py
@@ -8,8 +8,9 @@ class Mouvement:
         self.ENA = gpiozero.PWMOutputDevice(13)
 
         self.IN3 = gpiozero.DigitalOutputDevice(15)
-        self.IN4 = gpiozero.DigitalOutputDevice(14) # mouteur
+        self.IN4 = gpiozero.DigitalOutputDevice(14) # moteur D
         self.ENB = gpiozero.PWMOutputDevice(18)
+        self.initialise()
 
     def forward(self, wait=0):
         self.IN1.on()
@@ -19,8 +20,21 @@ class Mouvement:
         time.sleep(wait)
         self.initialise()   
 
-    def turn90(self, dir):
-        pass
+    def turn90(self, dir, condition=False):
+        while not condition:
+            if (dir == "left"):
+                self.IN2.on()
+                self.IN3.on()
+                self.ENA.on()
+                self.ENB.on()
+            elif (dir == "right"):
+                self.IN1.on()
+                self.IN4.on()
+                self.ENA.on()
+                self.ENB.on()
+            time.sleep(5)
+            condition = True
+        self.initialise()
 
     def initialise(self):
         self.ENA.off()
