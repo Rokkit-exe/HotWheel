43c492916b51ac92a22abc5adfa243c63bb00ce6
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-09 14:08:54 -0400

    Modif mouv

--- main.py
+++ main.py
@@ -4,33 +4,12 @@ from mouvement import Mouvement
 from infrarouge import InfraRouge
 Inf = sys.maxsize
 
-matrice_distances = [[0,   3,   1,   Inf, Inf, Inf],
-                     [3,   0,   1,   3,   Inf, Inf],
-                     [1,   1,   0,   3,   5,   Inf],
-                     [Inf, 3,   3,   0,   1,   3],
-                     [Inf, Inf, 5,   1,   0,   1],
-                     [Inf, Inf, Inf, 3,   1,   0]]
-
-matrice_distances = [ #1   2    3     4     5     6     7     8     9    10    11    12    13    14    15    16    17    18    19
-                     [ 0,  1,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [ 1,  0,   2,    3,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf, 2,   0,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf, 3,  Inf,   0,    1,    3,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,   1,    0,    3,   Inf,   3,   Inf,  Inf,   2,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,   3,   Inf,   0,    2,    1,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,   2,    0,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,   3,    1,   Inf,   0,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,   3,    0,    1,   Inf,  Inf,  Inf,  Inf,  Inf,   2,   Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   1,    0,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,   2,   Inf,  Inf,  Inf,  Inf,  Inf,   0,    2,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   2,    0,    1,   Inf,  Inf,  Inf,  Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   1,    0,    3,   Inf,   4,   Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   3,    0,    4,   Inf,   2,   Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   2,   Inf,  Inf,  Inf,  Inf,   4,    0,   Inf,  Inf,  Inf,   2],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   4,   Inf,  Inf,   0,   Inf,  Inf,  Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   2,   Inf,  Inf,   0,    2,    4],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   2,    0,   Inf],
-                     [Inf,Inf, Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,  Inf,   2,   Inf,   4,   Inf,   0]]
+matrice_distances =  [ [  0,   3,   1,   Inf, Inf, Inf ],  
+                       [  3,   0,   1,   3,   Inf, Inf ],
+                       [  1,   1,   0,   3,   5,   Inf ],
+                       [  Inf, 3,   3,   0,   1,   3   ],
+                       [  Inf, Inf, 5,   1,   0,   1   ],
+                       [  Inf, Inf, Inf, 3,   1,   0   ] ]
 
 graphe = Graphe(matrice_distances)
 
@@ -40,5 +19,5 @@ Infra = InfraRouge()
 M.turn("left", Infra, 1)
 
 
-# print(graphe.plus_court_chemin(0, 5)) # [0, 2, 3, 4, 5]
-# print(graphe.plus_court_chemin(5, 0)) # [5, 4, 3, 2, 0]
+#print(graphe.plus_court_chemin(0, 5)) # [0, 2, 3, 4, 5]
+#print(graphe.plus_court_chemin(5, 0)) # [5, 4, 3, 2, 0]
