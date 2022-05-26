f3d2b29851f8626dad3c65b2bd30e2e438628ec6
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-26 11:48:03 -0400

    Infrarouge

--- infrarouge.py
+++ infrarouge.py
@@ -3,33 +3,13 @@ import gpiozero
 class InfraRouge():
     def __init__(self):
         
-        # -------------  Sonars  --------------
-        #self.SGT = gpiozero.DigitalInputDevice(8, bounce_time=0.1)
-        #self.SGE = gpiozero.DigitalInputDevice(25, bounce_time=0.1)
-        #self.SDT = gpiozero.DigitalInputDevice(21, bounce_time=0.1)
-        #self.SDE = gpiozero.DigitalInputDevice(20, bounce_time=0.1)
-        
         self.IRG = gpiozero.DigitalInputDevice(23)
         self.IRD = gpiozero.DigitalInputDevice(24)
 
-        self.gauche_actif = None
-        self.droite_actif = None
-        #self.aucun_actif = not self.gauche_actif and not self.droite_actif if True else False
-
-        self.IRG.when_deactivated = self.actif_gauche
-        self.IRD.when_deactivated = self.actif_droite
-        self.IRG.when_activated = self.inactif_gauche
-        self.IRD.when_activated = self.inactif_droite
+        self.IRG.when_deactivated 
+        self.IRD.when_deactivated 
+        self.IRG.when_activated
+        self.IRD.when_activated
     
 
-    def actif_gauche(self):
-        self.gauche_actif = True
-        
-    def actif_droite(self):
-        self.droite_actif = True
-
-    def inactif_gauche(self):
-        self.gauche_actif = False
-        
-    def inactif_droite(self):
-        self.droite_actif = False
+   
