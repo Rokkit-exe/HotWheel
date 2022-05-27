5cc392959d9a4a1726bcf4a7028036a73979d308
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-27 12:50:05 -0400

    FDF update

--- infrarouge.py
+++ infrarouge.py
@@ -10,14 +10,13 @@ class InfraRouge():
         self.gauche_actif = None
         self.droite_actif = None
         self.doit_arreter = False
-        #self.IRG.when_deactivated = self.actif_gauche
-        #self.IRD.when_deactivated = self.actif_droite
-        #self.IRG.when_activated = self.inactif_gauche
-        #self.IRD.when_activated = self.inactif_droite
+        self.IRG.when_deactivated = self.actif_gauche
+        self.IRD.when_deactivated = self.actif_droite
+        self.IRG.when_activated = self.inactif_gauche
+        self.IRD.when_activated = self.inactif_droite
     
 
     def actif_gauche(self):
-        self.gauche_actif = True
         if(self.compteur_gauche == None):
             self.doit_arreter = False
             self.compteur_gauche = time.perf_counter()
@@ -29,7 +28,6 @@ class InfraRouge():
         
         
     def actif_droite(self):
-        self.droite_actif = True
         if(self.compteur_droite == None):
             self.doit_arreter = False
             self.compteur_droite = time.perf_counter()
@@ -40,11 +38,9 @@ class InfraRouge():
 
 
     def inactif_gauche(self):
-        time.sleep(0.2)
-        self.gauche_actif = False
+        pass
         
     def inactif_droite(self):
-        time.sleep(0.2)
-        self.droite_actif = False
+        pass
 
     
