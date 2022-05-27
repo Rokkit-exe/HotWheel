714728268dcdf026e7f7493106645f25d8474103
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-27 10:39:28 -0400

    FDF update

--- infrarouge.py
+++ infrarouge.py
@@ -22,11 +22,11 @@ class InfraRouge():
         self.droite_actif = True
 
     def inactif_gauche(self):
-        time.sleep(0.05)
+        time.sleep(0.07)
         self.gauche_actif = False
         
     def inactif_droite(self):
-        time.sleep(0.05)
+        time.sleep(0.07)
         self.droite_actif = False
 
     
