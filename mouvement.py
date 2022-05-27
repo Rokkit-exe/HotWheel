dba6e72c804ac19151f5dac83972cd75f0f78d08
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-27 13:14:17 -0400

    FDF update

--- mouvement.py
+++ mouvement.py
@@ -34,7 +34,7 @@ class Mouvement:
                     print("Correction droite")
                     self.Correction("droite")
 
-    def Correction(self, dir, wait=0.06):
+    def Correction(self, dir, wait=0.09):
         self.Initialise()
         if (dir == "gauche"):
             self.IN2.on()
