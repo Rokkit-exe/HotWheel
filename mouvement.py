24105463c7b39ced35b5c3af0311f7ad5ba5c9b2
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-27 10:24:17 -0400

    FDF update

--- mouvement.py
+++ mouvement.py
@@ -57,7 +57,7 @@ class Mouvement:
         time.sleep(wait)
     
     def Tourner(self, dir, est_detecter = False,  wait=0.5):
-        time.sleep(0.6)
+        time.sleep(0.4)
         self.Initialise()
         while(not est_detecter):
             if (dir == "gauche"):
