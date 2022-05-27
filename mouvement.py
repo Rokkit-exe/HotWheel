b14463c5ad74062c3f03085de4d25dc9739eaad1
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-27 13:55:56 -0400

    AC

--- mouvement.py
+++ mouvement.py
@@ -23,6 +23,7 @@ class Mouvement:
             self.ENA.value = 0.3
             self.ENB.value = 0.3
             time.sleep(0.01)
+            print(self.capteur_infrarouge.gauche_actif and self.capteur_infrarouge.droite_actif)
             if(self.capteur_infrarouge.gauche_actif and self.capteur_infrarouge.droite_actif):
                 print("J'arrête")
                 est_detecter = True
