2422dcf2c34b477cc90b8af35b636386d9e82a6d
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-30 13:07:11 -0400

    Modif

--- controleur.py
+++ controleur.py
@@ -29,6 +29,7 @@ class Controler:
             self.point_courant = self.chemin[index]
             self.prochain_point = self.chemin[index+1]
             print(self.chemin)
+            print('index: ', index)
 
             self.next_direction = self.get_direction(self.point_courant, self.prochain_point)
             if (self.cur_direction != self.next_direction):
